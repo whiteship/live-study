75331ab9e805be33d827ae0ac1b07564658b7bff
yeonsoo_lim <yslim6168@naver.com>
2020-12-07 21:42:46 +0900

    success

--- liveStudy_4th/__test__/gitHubIssuesTest.java
+++ liveStudy_4th/__test__/gitHubIssuesTest.java
@@ -1,12 +1,56 @@
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
+import org.kohsuke.github.GHRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-class githubReadMeTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class gitHubIssuesTest {
 
     @Test
-    void sum1() {
-        githubReadMe model = new githubReadMe();
-        assertEquals(model.sum(1,2), 3);
+    void gitHubConnectionWhiteship() throws IOException, ParseException {
+        gitHubIssues gitHub = new gitHubIssues();
+        GHRepository repository = gitHub.gitHubConnection("whiteship/live-study");
+        assertEquals(repository.getDescription(), "온라인 스터디");
+    }
+
+    @Test
+    void checkIssuesSize() throws IOException, ParseException {
+        gitHubIssues gitHub = new gitHubIssues();
+        gitHub.gitHubConnection("whiteship/live-study");
+        assertEquals(gitHub.getIssues().size(), 18);
+    }
+
+    @Test
+    void getCrewList(){
+
+    }
+
+    @Test
+    void writeFile(){
+        String path = "resources/tempList.txt";
+        Map<String, Integer> tempList = new HashMap<String, Integer>();
+        tempList.put("image",1);
+        tempList.put("maven", 10);
+        tempList.put("ysl",18);
+
+        File file = new File(path);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            for(String key : tempList.keySet()){
+                writer.write(String.format("ID : %s  -> 참여율 : %.2f\n", key, (float)tempList.get(key) / 18 * 100));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(file.exists());
     }
 }
--- liveStudy_4th/src/main/java/gitHubIssues.java
+++ liveStudy_4th/src/main/java/gitHubIssues.java
@@ -1,19 +1,72 @@
-public class githubReadMe {
+import org.json.simple.parser.ParseException;
+import org.kohsuke.github.*;
 
-    public int sum(int x, int y){
-        return x+y;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class gitHubIssues {
+    GitHub gitHub;
+    GHRepository repository;
+
+    private String getToken() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        JSONObject token = (JSONObject) parser.parse(new FileReader("config/config.json"));
+        return (String) token.get("token");
     }
-    
-    GitHub github = GitHub.connect();
 
-    GHRepository repo = github.createRepository(
-            "new-repository","this is my new repository",
-            "https://www.kohsuke.org/",true/*public*/);
-repo.addCollaborators(github.getUser("abayer"),github.getUser("rtyler"));
-repo.delete();
-    public static void main(String[] args) {
-        githubReadMe model = new githubReadMe();
-        System.out.println(model.sum(5,6));
-        System.out.println("here");
+    public GHRepository gitHubConnection(String name) throws IOException, ParseException {
+        gitHub = new GitHubBuilder().withOAuthToken(this.getToken()).build();
+        repository = gitHub.getRepository(name);
+        return repository;
+    }
+
+    public List<GHIssue> getIssues() throws IOException {
+        return repository.getIssues(GHIssueState.valueOf("ALL"));
+    }
+
+    public Map<String, Integer> getCrewList(Map<String, Integer> crewList, List<GHIssue> issues) throws IOException {
+        for(GHIssue issue : issues){
+            PagedIterable<GHIssueComment> lists = issue.listComments();
+            for(GHIssueComment list : lists){
+                String crew = list.getUser().getLogin();
+                if(crewList.containsKey(crew)){
+                    crewList.put(crew,crewList.get(crew)+1);
+                }else {
+                    crewList.put(crew, 1);
+                }
+            }
+        }
+        return crewList;
+    }
+
+    public void writeFile(String path, Map<String, Integer> crewList){
+        File file = new File(path);
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            for (String key : crewList.keySet()) {
+                writer.write(String.format("ID : %s  -> 참여율 : %.2f\n", key, (float)crewList.get(key) / 18 * 100));
+            }
+            writer.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
+        Map<String, Integer> crewList = new HashMap<String, Integer>();
+        gitHubIssues gitHub = new gitHubIssues();
+
+        gitHub.gitHubConnection("whiteship/live-study");
+
+        List<GHIssue> issues = gitHub.getIssues();
+
+        crewList = gitHub.getCrewList(crewList, issues);
+
+        gitHub.writeFile("resources/crewList.txt", crewList);
     }
 }
